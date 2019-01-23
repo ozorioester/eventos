92c2119b2defbcf89c0a64568eaa249e4656af13
Ester <ozorio.ester@icloud.com>
2019-01-22 22:02:39 -0200

    data/hora1

--- js/eventos.js
+++ js/eventos.js
@@ -1,15 +1,26 @@
+//baixa o conteudo json e cria a variavel dados
 $.getJSON("model/eventos_listar.php", function(dados){
 
+    //para cada item em dados gera um valor
     dados.forEach(function(valor){
 
+        var data = valor.datahora.split(" ");
+        var dia = data[0].split("-");
+        var hora = data[1].split(":");
+
+        var dataFormatado = dia[2]+"/"+dia[1]+"/"+dia[0];
+        dataFormatado+= " "+hora[0]+":"+hora[1];
+
+        //gerando a linha com os dados
         var tr = "<tr>"
             +"<td>"+ valor.id +"</td>"
             +"<td>"+ valor.nome +"</td>"
-            +"<td>"+ valor.datahora +"</td>"
+            +"<td>"+ dataFormatado +"</td>"
             +"<td>"+ valor.local +"</td>"
             +"<td> [Editar] [Deletar]</td>"
             +"</tr>";
 
+            //adiciona na tabela
             $("#lista-eventos").append(tr);
 
     });//fim do forEach
